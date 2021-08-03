0e8734100caa089e5f6733199e1a1247a9170df5
patrc <patricia.roz@outlook.com>
2021-08-03 20:03:43 -0300

    First commit

---
+++ src/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		// System.out.println("Hello world!");
+		
+		Gato gato = new Gato(null, null, null);
+		System.out.println(gato);
+	}
+
+}
